f571263718f6d950ad900406d9c5ae73d5021d42
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2023-05-08 20:08:35 -0500

    fix: env for deploy

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const URL =
-  import.meta.env.NODE_ENV === "production"
+  // eslint-disable-next-line no-undef
+  process.env.NODE_ENV === "production"
     ? import.meta.env.VITE_BACKEND_URL
     : "http://localhost:8000";
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.NODE_ENV === "production" ? "/static/" : "/",
+  // eslint-disable-next-line no-undef
+  base: process.env.NODE_ENV === "production" ? "/static/" : "/",
 });
