d49f0d734d7d30eea85651f8eace55ba15b89421
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2023-05-08 21:09:58 -0500

    fix: url endpoints

--- client/src/pages/TasksFormPage.jsx
+++ client/src/pages/TasksFormPage.jsx
@@ -55,7 +55,7 @@ const TasksFormPage = () => {
       })
     }
 
-    navigate('/tasks');
+    navigate('/');
   });
 
   return (
--- tasks/urls.py
+++ tasks/urls.py
@@ -8,6 +8,6 @@ router = routers.DefaultRouter()
 router.register(r"tasks", views.TaskView, "task")
 
 urlpatterns = [
-    path("/", include(router.urls)),
+    path("", include(router.urls)),
     path("tasks/docs/", include_docs_urls(title="Tasks API")),
 ]
